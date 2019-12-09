9ce9c2a6635fa0bdbf0a9dfb9cc1910211497708
Grzegorz Latosinski <glatosinski@antmicro.com>
2019-12-09 14:54:37 +0100

    Updated the QL732B CSV and CMD files
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- ql732b/QL732B_cdl.cmd
+++ ql732b/QL732B_cdl.cmd
@@ -42,13 +42,13 @@ define_WL_tst_prefix wl_tst
 define_BL_tst_prefix bl_tst
 
 # define WL/BL banks
-define_WL_bank 0 -range {0 421} -WL_prefix {ttl_wl ttr_wl} -parent psm -rows {0 14}
-define_WL_bank 1 -range {0 421} -WL_prefix {tbl_wl tbr_wl} -parent psm -rows {15 29}
+define_WL_bank 0 -range {0 421} -WL_prefix {ttl_wl ttr_wl} -parent psm -rows {0 15}
+define_WL_bank 1 -range {0 421} -WL_prefix {tbl_wl tbr_wl} -parent psm -rows {16 31}
 define_BL_bank 0 -range {0 357} -BL_prefix {tl_tbl tl_bbl bl_tbl bl_bbl} -parent psm  -cols {0 16}
 define_BL_bank 1 -range {0 357} -BL_prefix {tr_tbl tr_bbl br_tbl br_bbl} -parent psm  -cols {17 33}
 
 # row/WL information
-define_row {0 29} -WL_range {{1 28},{29 56},{57 84},{85 112},{113 140},{141 168},{169 196},{197 224},{225 252},{253 281},{282 309},{310 337},{338 365},{366 393},{394 421},{422 449},{450 477},{478 505},{506 533},{534 561},{562 589},{590 617},{618 645},{646 674},{675 702},{703 730},{731 758},{759 786},{787 814},{815 842}}
+define_row {0 31} -WL_range {{},{1 28},{29 56},{57 84},{85 112},{113 140},{141 168},{169 196},{197 224},{225 252},{253 281},{282 309},{310 337},{338 365},{366 393},{394 421},{422 449},{450 477},{478 505},{506 533},{534 561},{562 589},{590 617},{618 645},{646 674},{675 702},{703 730},{731 758},{759 786},{787 814},{815 842},{}}
 define_column {0 33} -BL_range {{1 21},{22 42},{43 63},{64 84},{85 105},{106 126},{127 147},{148 168},{169 189},{190 210},{211 231},{232 252},{253 273},{274 294},{295 315},{316 336},{337 357},{358 378},{379 399},{400 420},{421 441},{442 462},{463 483},{484 504},{505 525},{526 546},{547 567},{568 588},{589 609},{610 630},{631 651},{652 672},{673 693},{694 714}}
 
 # define instance prefix for various types of instances
