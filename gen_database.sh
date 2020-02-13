13aca04e203f4e192c3f904ea15905a1fca5dfc9
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-02-13 18:14:45 +0100

    Modified gen_database.sh to support submodule and new paths
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- gen_database.sh
+++ gen_database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 DEV=ql732b
-SCRIPT=./quicklogic/convert_csv_to_db.py
-DEVICEXML=./$DEV/QLAL4S3B.xml
-CSVLOCDIR=./$DEV/csv
+SCRIPT=./quicklogic_fasm/convert_csv_to_db.py
+DEVICEXML="./EOS-S3/Device Architecture Files/QLAL4S3B.xml"
+CSVLOCDIR="./EOS-S3/Configuration Bitstream Files"
 
 
 CSVLOC=${CSVLOCDIR%/}
@@ -17,17 +17,17 @@ OUTDIR=${1%/}
 
 rm -rf $OUTDIR/*
 
-python3 $SCRIPT $CSVLOC/DeviceMacroCoord_$DEV.csv $OUTDIR/macro.db \
+python3 $SCRIPT "$CSVLOC/DeviceMacroCoord_$DEV.csv" "$OUTDIR/macro.db" \
    --include \
-       $CSVLOC/macroTable_$DEV.csv \
-       $CSVLOC/macro_clkTable_$DEV.csv \
-       $CSVLOC/macro_gclkTable_$DEV.csv \
-       $CSVLOC/macro_interfaceTable_$DEV.csv \
-       $CSVLOC/macro_interface_topTable_$DEV.csv \
-       $CSVLOC/macro_interface_top_rightTable_$DEV.csv \
-       $CSVLOC/macro_interface_top_leftTable_$DEV.csv \
-       $CSVLOC/macro_interface_rightTable_$DEV.csv \
-       $CSVLOC/macro_interface_leftTable_$DEV.csv \
+       "$CSVLOC/macroTable_$DEV.csv" \
+       "$CSVLOC/macro_clkTable_$DEV.csv" \
+       "$CSVLOC/macro_gclkTable_$DEV.csv" \
+       "$CSVLOC/macro_interfaceTable_$DEV.csv" \
+       "$CSVLOC/macro_interface_topTable_$DEV.csv" \
+       "$CSVLOC/macro_interface_top_rightTable_$DEV.csv" \
+       "$CSVLOC/macro_interface_top_leftTable_$DEV.csv" \
+       "$CSVLOC/macro_interface_rightTable_$DEV.csv" \
+       "$CSVLOC/macro_interface_leftTable_$DEV.csv" \
    --macro-names \
        macro \
        macro_clk \
@@ -38,8 +38,8 @@ python3 $SCRIPT $CSVLOC/DeviceMacroCoord_$DEV.csv $OUTDIR/macro.db \
        macro_interface_top_left \
        macro_interface_right \
        macro_interface_left \
-   --techfile $DEVICEXML \
-   --routing-bits-outfile $OUTDIR/macro-routing.db
+   --techfile "$DEVICEXML" \
+   --routing-bits-outfile "$OUTDIR/macro-routing.db"
 
-python3 $SCRIPT $CSVLOC/ColClk_$DEV.csv $OUTDIR/colclk.db
-python3 $SCRIPT $CSVLOC/TestMacro_$DEV.csv $OUTDIR/testmacro.db
+python3 $SCRIPT "$CSVLOC/ColClk_$DEV.csv" "$OUTDIR/colclk.db"
+python3 $SCRIPT "$CSVLOC/TestMacro_$DEV.csv" "$OUTDIR/testmacro.db"
